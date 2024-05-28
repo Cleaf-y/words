fd8f9e3082a8c5e9909df4e31f835b669bcb1d44
cdeeef <5286cleaff@gmail.com>
2024-05-28 23:05:15 +0800

    add basic layout for main page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
-
+import { useConfigStore } from "./stores/config";
+const configStore = useConfigStore();
 </script>
 
 <template>
-<n-space>
-  <n-button type="primary">123</n-button>
-</n-space>
+  <n-config-provider :theme="configStore.currentTheme">
+    <n-message-provider>
+      <n-dialog-provider>
+        <n-notification-provider>
+          <n-modal-provider>
+            <n-layout class="base">
+              <router-view></router-view>
+            </n-layout>
+          </n-modal-provider>
+        </n-notification-provider>
+      </n-dialog-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
+
+<style>
+.base {
+  height: 100vh;
+  width: 100vw;
+  padding: 12px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: auto;
+}
+</style>
