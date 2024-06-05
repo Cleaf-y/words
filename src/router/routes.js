f317d0d6094f7707f175bca8b894ea482a276bfa
cdeeef <5286cleaff@gmail.com>
2024-06-05 23:46:33 +0800

    add: 添加了设置页面路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,10 +8,16 @@ export const routes = [
     path: '/query',
     name: 'Query',
     component: () => import('@/pages/Query.vue')
+  },
   {
     path: '/book',
     name: 'WordsBook',
     component: () => import('@/pages/WordsBook.vue')
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('@/pages/Settings.vue')
   },
   }
 ];
