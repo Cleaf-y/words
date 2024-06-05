dd4c7e789a0854203d23d39c0d1618f16016388b
cdeeef <5286cleaff@gmail.com>
2024-06-05 23:39:59 +0800

    refactor：该用封装的fetch API重构为异步函数

--- src/api/qoute.js
+++ src/api/qoute.js
@@ -1,9 +1,8 @@
-import axios from "axios";
-import axiosTauriApiAdapter from "axios-tauri-api-adapter";
+import {httpRequest} from "@/api/base.js";
 import dayjs from "dayjs";
 
 
-function getDailyQoute() {
+async function getDailyQuote() {
     const todayStr = dayjs().format('YYYY-MM-DD')
     let lastUpdateDateStr = localStorage.getItem('lastUpdateDate')
     if (lastUpdateDateStr === todayStr) {
@@ -12,24 +11,21 @@ function getDailyQoute() {
     }
     console.debug('fetching new qoute')
 
-    const client = axios.create({adapter: axiosTauriApiAdapter});
-
-    client.get('https://open.iciba.com/dsapi/', {
-        params:{
+    const result = await httpRequest({
+        url: 'https://open.iciba.com/dsapi/',
+        qurey: {
             date: todayStr
         }
     })
-        .then(res => {
-            let {content, note} = res.data
-            localStorage.setItem('lastUpdateDate', todayStr)
-            localStorage.setItem('lastUpdateContent', JSON.stringify({
-                content, note
-            }))
-            return {content, note}
-        })
-        .catch(e => {
-            console.log(e)
-        })
+
+    console.log(result)
+
+    let {content, note} = result.data
+    localStorage.setItem('lastUpdateDate', todayStr)
+    localStorage.setItem('lastUpdateContent', JSON.stringify({
+        content, note
+    }))
+    return {content, note}
 }
 
-export {getDailyQoute}
+export {getDailyQuote}
