d554977001b81e86e6812f29b52c996d6402cd99
cdeeef <5286cleaff@gmail.com>
2024-06-05 23:50:40 +0800

    add: encapsulated a status display component

---
+++ src/components/Status.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref, reactive} from 'vue'
+
+import {AccessTimeOutlined, ErrorOutlineOutlined} from "@vicons/material";
+
+const props = defineProps({
+  status: Number
+})
+
+const statusMap = {
+  0: '查询中',
+  1: '无内容'
+}
+</script>
+
+<template>
+<n-empty id="tips" :description="statusMap[props.status]" size="huge">
+  <template #icon>
+    <n-icon v-if="props.status === 0"><AccessTimeOutlined /></n-icon>
+    <n-icon v-if="props.status === 1"><ErrorOutlineOutlined /></n-icon>
+  </template>
+</n-empty>
+</template>
+
+<style scoped lang="scss">
+#tips {
+  margin-top: 150px;
+}
+</style>
