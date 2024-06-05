d75ebf565433252983950910e491e6576e2932a5
cdeeef <5286cleaff@gmail.com>
2024-06-05 23:32:50 +0800

    add: App启动时的数据库初始化

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
 import { useConfigStore } from "./stores/config";
 import MainWrapper from "@/components/MainWrapper.vue";
 const configStore = useConfigStore();
+
+
+import {ref, onBeforeMount} from 'vue'
+import {initDatabase} from "@/database/init.js";
+
+const isReady = ref(false)
+onBeforeMount(async ()=>{
+  await initDatabase()
+  isReady.value = true
+})
 </script>
 
 <template>
-  <n-config-provider :theme="configStore.currentTheme">
+  <n-config-provider :theme="configStore.currentTheme" v-if="isReady">
     <n-message-provider>
       <n-dialog-provider>
         <n-notification-provider>
