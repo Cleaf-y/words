e76ce650286d7114d85c0c3e89be92b4fe16c7a5
cdeeef <5286cleaff@gmail.com>
2024-06-05 23:33:34 +0800

    add: 封装从GPT返回的结果中反序列化JSON的方法

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 import { fetch, ResponseType, Body } from '@tauri-apps/api/http'
 
 // https://tauri.app/zh-cn/v1/api/js/http#fetch
-export const httpRequest = (opts = {}) => {
+const httpRequest = (opts = {}) => {
     return new Promise((resolve, reject) => {
         const { url, method, query, data, headers, callback } = opts
         fetch(url, {
@@ -26,3 +26,42 @@ export const httpRequest = (opts = {}) => {
             })
     })
 }
+
+
+function getObjectFromString(str) {
+    let jsonStr = null
+
+    // 尝试匹配 json object
+    let match = str.match(/\{[\s\S]*\}/)
+    if (match) {
+        jsonStr = match[0]
+        try {
+            return JSON.parse(jsonStr)
+        } catch (error) {
+            match = undefined
+        }
+    }
+
+    // 尝试匹配 ```json ```包裹中匹配
+    const regex = /```json\n([\s\S]*?)\n```/
+    match = str.match(regex)
+    if (match) {
+        jsonStr = match[1]
+
+        try {
+            return JSON.parse(jsonStr)
+        } catch (error) {
+            match = undefined
+        }
+    }
+
+    // 直接尝试解析
+    try {
+        return JSON.parse(jsonStr)
+    } catch (error) {
+        return null
+    }
+}
+
+
+export { getObjectFromString, httpRequest}
