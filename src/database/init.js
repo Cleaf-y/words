bd266a96d839e2947541eb013d8bbf656eb04ff0
cdeeef <5286cleaff@gmail.com>
2024-06-07 20:55:17 +0800

    add: check if database is created on App launch

--- src/database/init.js
+++ src/database/init.js
@@ -1,7 +1,29 @@
-import Datebase from 'tauri-plugin-sql-api'
+import Datebase from "tauri-plugin-sql-api";
 
+export async function initDatabase() {
+  const db = await Datebase.load("sqlite:words.db");
 
-export async function initDatabase(){
-    const db = await Datebase.load("sqlite:words.db")
-    console.log(db)
+  // check if table exists
+  try {
+    const result = await db.execute("SELECT * FROM QueryHistory");
+    console.log("Table exists");
+    return;
+  } catch (err) {
+    if (err.includes("code: 1")) {
+      console.log("Table does not exist");
+      await createTables();
+    }
+  }
+}
+
+async function createTables() {
+  const db = await Datebase.load("sqlite:words.db");
+  const createTableSqlList = [
+    "CREATE TABLE QueryHistory (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, addTimestamp TEXT);",
+    "CREATE TABLE Note (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT, importance INTEGER, addTimestamp TEXT);",
+    "CREATE TABLE Revision (id INTEGER PRIMARY KEY AUTOINCREMENT, note_id INTEGER, expectedTimestamp TEXT, isFinished INTEGER, finishedTimestamp TEXT, FOREIGN KEY (note_id) REFERENCES Note(id));",
+  ];
+  createTableSqlList.forEach(async (sql) => {
+    await db.execute(sql);
+  });
 }
